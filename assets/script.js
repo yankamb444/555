09e795dde5d8dd9326951be7cc31c2f6a65d92c6
Yandra Kamberaj <yandrakamb@gmail.com>
2023-04-24 19:03:07 -0400

    everything is finished, just update readme

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-function date(){
+
+$(document).ready(function(){
+  function date(){
     let date = document.querySelector("#currentDay");
     let todaysDate = new Date();
     const month = todaysDate.toLocaleString("default");
@@ -8,47 +10,42 @@ function date(){
     date()
     
     
-    function timeBlocks(num, classHour){
-      return(
-    `<div id="hour-${num}" class="row time-block ${classHour}">
-            <div class="col-2 col-md-1 hour text-center py-3">${num}</div>
-            <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-            <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-              <i class="fas fa-save" aria-hidden="true"></i>
-            </button>
-          </div>
-    `
-      )
-    }
-    
-    function colorChange() {
-    
-    }
-    
     function calofHours(){
-      let todaysDate = new Date()
-        const currentHour = todaysDate.getHours()
+      let currentHour = dayjs().hour()
+      $(".time-block").each(function(){
+        let timeblockhour = parseInt($(this).attr("id").split("-")[1])
+        if (timeblockhour < currentHour) {
+          $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (timeblockhour === currentHour){
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+        }
+        else {
+          $(this).removeClass("past");
+            $(this).addClass("future");
+            $(this).removeClass("present");
+        }
+
+      })
     }
-    
-    for (let i = 0; i < 1; i++) {
-        if(num === currentHour) {
-            $(classHour).removeClass("past");
-            $(classHour).removeClass("future");
-            $(classHour).addClass("present");
-          }
-          else if (num<currentHour) {
-            $(classHour).addClass("past");
-            $(classHour).removeClass("future");
-            $(classHour).removeClass("present");
-          }
-          else (num>currentHour); {
-            $(classHour).removeClass("past");
-            $(classHour).addClass("future");
-            $(classHour).removeClass("present");
-          }
-      }
-    
-    
-    
-    
-        // need to display num and hour \
+    calofHours()
+
+    setInterval(calofHours,10000)
+
+    // save button function that will save text entered 
+$('.saveBtn').on('click', function () {
+  let text = $(this).parent().attr('id');
+  let entered = $(this).siblings('.description').val();
+  localStorage.setItem(text, entered);
+
+});
+let hours = ["9","10","11","12","13","14","15","16","17"]
+hours.forEach(hour=>{
+  $(`#hour-${hour} .description`).val(localStorage.getItem(`hour-${hour}`))
+})
+})
+
